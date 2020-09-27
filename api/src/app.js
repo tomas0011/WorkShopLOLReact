90d04d556cda7647d8ea56b3de59b74ce882a02c
tomas0011 <tomasbenjamin117@gmail.com>
2020-09-27 20:56:34 -0300

    SIN PACKAGE

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 // import middlewares
 const bodyPaser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
 // config
 const server = express()
@@ -13,6 +14,12 @@ const cardsPath = require('./routes/champ')
 // middlewares
 server.use(bodyPaser.json())
 server.use(morgan("dev"));
+server.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000',
+    allowedHeaders: "Origin, X-Requested-With, Content-Type, X-Auth-Token, X-PINGOTHER, Accept",
+    methods: "GET,HEAD,PUT,PATCH,OPTIONS,POST,DELETE"
+}))
 
 // middlewares routes
 server.use('/champ', cardsPath)
